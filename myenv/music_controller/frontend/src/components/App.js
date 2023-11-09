61af05b2361fe3c1c56a9ab1ec6c69c74d1ffadd
jackmaple2 <jackmaple3000@gmail.com>
2023-11-09 23:46:00 +0000

    adds homepage file

--- myenv/music_controller/frontend/src/components/App.js
+++ myenv/music_controller/frontend/src/components/App.js
@@ -9,7 +9,7 @@ export default class App extends Component {
     render() {
         return (
             <div>
-                <h1>Testing react code</h1>
+                <h1>Test website</h1>
             </div>
         )
     } 
